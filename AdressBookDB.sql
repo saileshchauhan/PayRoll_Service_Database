fb1c3eaa135dad0605823fca67cfb99695b0706d
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-21 17:35:44 +0530

    [sailesh] Add:CREATE

--- AdressBookDB.sql
+++ AdressBookDB.sql
@@ -3,3 +3,19 @@ create database AddressBook_Service
 select * from sys.databases;
 
 use AddressBook_Service;
+
+SELECT DB_NAME()
+
+CREATE table AddressBook
+(
+Id int Identity(1,1) not null,
+FirstName varchar(100)  not null,
+LastName varchar(100) not null,
+Adress varchar(100) not null,
+City varchar(100) not null,
+State varchar(100) not null,
+Zip varchar(100) not null,
+PhoneNumber int not null,
+Email varchar(50) not null
+) 
+
