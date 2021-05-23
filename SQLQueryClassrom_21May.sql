f43d30a14a77d3c64c4a7dce1a34355fcc17d45b
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-23 16:41:48 +0530

    [sailesh] Add: UC3-INSERT RECORDS

--- SQLQueryClassrom_21May.sql
+++ SQLQueryClassrom_21May.sql
@@ -15,6 +15,12 @@ start date not null,
 gender varchar(1) not null,
 );
 
+INSERT INTO employee_payroll1 VALUES
+('tom',10000,'2018-02-05','M'),
+('harry',23000,'2018-05-16','F'),
+('sam',34569,'2017-05-23','M'),
+('sandra',78000,'2019-03-02','F')
+
 
 
 SELECT * from employee_payroll1
