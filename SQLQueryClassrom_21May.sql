6d6263b70573a53084b42a1f5318868bf78fd4e8
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-23 16:34:22 +0530

    [sailesh] Add:UC2-create table

--- SQLQueryClassrom_21May.sql
+++ SQLQueryClassrom_21May.sql
@@ -4,4 +4,17 @@ select * from sys.databases;
 
 use PayRoll_Service13;
 
-select DB_NAME();
+select DB_NAME()
+
+create table employee_payroll1
+(
+Id int identity(1,1) primary key,
+name varchar(100) not null,
+salary money not null,
+start date not null,
+gender varchar(1) not null,
+);
+
+
+
+SELECT * from employee_payroll1
